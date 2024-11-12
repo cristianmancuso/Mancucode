786f2baf59639b64faac206467c0a74614465858
LosMancuso <emilysabean@Emilys-MacBook-Pro.local>
2024-11-12 15:24:43 -0500

    actualizacion config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Otras opciones de configuración
 };
 
 export default nextConfig;
